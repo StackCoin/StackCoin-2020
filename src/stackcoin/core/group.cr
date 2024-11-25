e8a964fd9ff9df37640b887e04313107d3b0cf86
Jack Arthur Harrhy <me@jackharrhy.com>
2024-11-25 12:47:15 -0330

    code i wrote 3 years ago oh wow

--- src/stackcoin/core/group.cr
+++ src/stackcoin/core/group.cr
@@ -9,6 +9,12 @@ class StackCoin::Core::Group
     class InvalidGroupChannel < Failure
     end
 
+    class NoSuchUserAccount < Failure
+    end
+
+    class NotAuthorized < Failure
+    end
+
     class ValidGroupChannel < Success
     end
   end
@@ -34,10 +40,30 @@ class StackCoin::Core::Group
                            query_valid_channel(guild_id)
                          end
 
-    unless designated_channel && designated_channel == channel_id
+    unless designated_channel
       return Result::NoDesignatedChannel.new("No designated channel set")
     end
 
+    unless designated_channel == channel_id
+      return Result::NotAuthorized.new("Incorrect channel for messages, please use #{channel_id}")
+    end
+
     Result::ValidGroupChannel.new("Valid group channel")
+  end
+
+  def self.set_group_channel(tx : ::DB::Transaction, invokee_id : Int32?, guild_id : Discord::Snowflake, channel_id : Discord::Snowflake?)
+    unless invokee_id.is_a?(Int32)
+      return Result::NoSuchUserAccount.new("You doesn't have a user account")
+    end
+
+    cnn = tx.connection
+
+    invokee_is_admin = cnn.query_one(<<-SQL, invokee_id, as: Bool)
+      SELECT admin FROM "user" WHERE id = $1
+      SQL
+
+    unless chanel_id
+      # TODO remove
+    end
   end
 end
